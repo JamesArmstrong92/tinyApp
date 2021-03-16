9dd3c19c34e94572caf0fe974a604114ddad297f
JamesArmstrong92 <jarmst23@gmail.com>
2021-03-15 22:18:48 -0400

    Added additional GET and POST requests

--- express_server.js
+++ express_server.js
@@ -34,20 +34,31 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
 
   res.render("urls_index" ,templateVars);
-
 });
 
 app.get("/urls/new", (req, res) => {
 
   res.render("urls_new");
-
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL].longURL
+  };
+  
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  
+  const longURL = 'http://' + urlDatabase[req.params.shortURL].longURL;
+  console.log('test --->', urlDatabase[req.params.shortURL]);
+  console.log('longURL --->', longURL);
+  
+  res.redirect(longURL);
+
+});
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
@@ -57,9 +68,17 @@ app.get("/", (req, res) => {
 });
 
 
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  
+  const shortURL = generateRandomString();
+
+  urlDatabase[shortURL] = {
+    longURL: req.body.longURL,
+
+  };
+
+  res.redirect('/urls/' + shortURL);
 });
 
 app.listen(PORT, () => {
