e575d0d90cef6f54497dc6ed2a0651f9e9485e04
JamesArmstrong92 <jarmst23@gmail.com>
2021-03-16 13:01:04 -0400

    Fixed bug with urls homepage

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -8,77 +8,84 @@ app.set("view engine", "ejs");
 
 
 
-const generateRandomString = function(str) {
+const generateRandomString = function() {
 
   let output = '';
   let randomString = '1234567890abcdefghijklmnopqrstuvwxyz';
-  
   for (let i = 0; i < 6; i++) {
     output += randomString.charAt(Math.floor(Math.random() * randomString.length));
   }
   return output;
 };
 
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+//GET requests
+
+app.get("/", (req, res) => {
+  
+  res.send("Hello!");
+
 });
 
-app.get("/urls", (req, res) => {
+app.get("/urls.json", (req, res) => {
+  
+  res.json(urlDatabase);
 
-  const templateVars = { urls: urlDatabase };
-
-  res.render("urls_index" ,templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
 
   res.render("urls_new");
+
+});
+app.get("/urls", (req, res) => {
+  
+  const templateVars = {
+    urls: urlDatabase
+  };
+  res.render("urls_index" ,templateVars);
+
 });
 
+
 app.get("/urls/:shortURL", (req, res) => {
+  
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL
   };
-  
   res.render("urls_show", templateVars);
+
 });
 
 app.get("/u/:shortURL", (req, res) => {
   
   const longURL = 'http://' + urlDatabase[req.params.shortURL].longURL;
-  console.log('test --->', urlDatabase[req.params.shortURL]);
-  console.log('longURL --->', longURL);
   
   res.redirect(longURL);
 
 });
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-
+//Post responses
 
 app.post("/urls", (req, res) => {
   
   const shortURL = generateRandomString();
-
-  urlDatabase[shortURL] = {
-    longURL: req.body.longURL,
-
-  };
-
+  
+  urlDatabase[shortURL] = 'http://' + req.body.longURL;
+  
   res.redirect('/urls/' + shortURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+
 });
 
 app.listen(PORT, () => {
